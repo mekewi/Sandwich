883ff150e5fd7668ea475fcdf8f7e31a4d6643f5
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 00:20:14 +0200

    Swipe Detector
    Add Swipe Detector that detect swipe and fire an Event with swipe data

---
+++ Assets/Scripts/SwipeDetector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using UnityEngine;
+
+public class SwipeDetector : MonoBehaviour
+{
+    private Vector2 fingerDownPosition;
+    private Vector2 fingerUpPosition;
+
+    [SerializeField]
+    private float minDistanceForSwipe = 20f;
+
+    private void Update()
+    {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+		    fingerUpPosition = Input.mousePosition;
+		    fingerDownPosition = Input.mousePosition;
+	    }
+
+	    if (Input.GetMouseButtonUp(0))
+	    {
+		    fingerDownPosition = Input.mousePosition;
+		    DetectSwipe();
+	    }
+    }
+
+    private void DetectSwipe()
+    {
+        if (SwipeDistanceCheckMet())
+        {
+            if (IsVerticalSwipe())
+            {
+                var direction = fingerDownPosition.y - fingerUpPosition.y > 0 ? Direction.Up : Direction.Down;
+                SendSwipe(direction);
+            }
+            else
+            {
+                var direction = fingerDownPosition.x - fingerUpPosition.x > 0 ? Direction.Right : Direction.Left;
+                SendSwipe(direction);
+            }
+            fingerUpPosition = fingerDownPosition;
+        }
+    }
+
+    private bool IsVerticalSwipe()
+    {
+        return VerticalMovementDistance() > HorizontalMovementDistance();
+    }
+
+    private bool SwipeDistanceCheckMet()
+    {
+        return VerticalMovementDistance() > minDistanceForSwipe || HorizontalMovementDistance() > minDistanceForSwipe;
+    }
+
+    private float VerticalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.y - fingerUpPosition.y);
+    }
+
+    private float HorizontalMovementDistance()
+    {
+        return Mathf.Abs(fingerDownPosition.x - fingerUpPosition.x);
+    }
+
+    private void SendSwipe(Direction direction)
+    {
+        SwipeData swipeData = new SwipeData()
+        {
+            Direction = direction,
+            StartPosition = fingerDownPosition,
+            EndPosition = fingerUpPosition
+        };
+        GameEventsHub.Instance.Notify<SwipeDetectedEvent>(swipeData);
+    }
+}
+
+public struct SwipeData
+{
+    public Vector2 StartPosition;
+    public Vector2 EndPosition;
+    public Direction Direction;
+}
+
+public enum Direction
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3
+}
