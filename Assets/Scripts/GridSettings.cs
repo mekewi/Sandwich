7794ec124a18dec4ddc31e01850bd0867fdc23e1
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 00:22:59 +0200

    Grid Settings
    Add Grid Settings Scriptable object

---
+++ Assets/Scripts/GridSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewGridSetting", menuName = "Settings/GridSettings")]
+public class GridSettings : ScriptableObject
+{
+	public IntVector2 gridSize;
+	public float spaceBetweenCells;
+	public List<GameObject> gamePlayCells;
+	public GameObject baseCellPrefab;
+}
+[Serializable]
+public struct IntVector2
+{
+	public int x;
+	public int y;
+}
