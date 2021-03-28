5af5ab8c1da22c48a0ef6dc2c96bf9eec2818b6c
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 00:19:26 +0200

    Swipe Event
    Add swipe event that event

---
+++ Assets/Scripts/Events/SwipeDetectedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "SwipeDetectedEvent", menuName = "Events/SwipeDetectedEvent")]
+public class SwipeDetectedEvent : Observable<SwipeDetectedEvent>
+{
+	public SwipeData swipeData;
+    public override void SetData(params object[] data)
+    {
+	    swipeData = (SwipeData)data[0];
+    }
+}
