a37f28af92de44b02228969e71ce56cb1742eebd
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 04:58:45 +0200

    create ui manager script to manage ui events in game scene

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class UIManager : MonoBehaviour
+{
+    public Text gameState;
+    void Start()
+    {
+        GameEventsHub.Instance.Register<WinLoseEvent>(OnWinLose);
+    }
+    private void OnDestroy()
+    {
+        GameEventsHub.Instance.UnRegister<WinLoseEvent>(OnWinLose);
+    }
+    private void OnWinLose(WinLoseEvent obj)
+    {
+        gameState.text = obj.isWin ? "Winner" : "Loser";
+    }
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoHome()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
