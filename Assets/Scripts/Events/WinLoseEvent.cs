8e34afba64e3e72a2c2e2fdc660457690cb630f3
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 04:55:51 +0200

    add win lose event

---
+++ Assets/Scripts/Events/WinLoseEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "WinLoseEvent", menuName = "Events/WinLoseEvent")]
+public class WinLoseEvent : Observable<WinLoseEvent>
+{
+    public bool isWin;
+    public override void SetData(params object[] data)
+    {
+        isWin = (bool) data[0];
+    }
+}
