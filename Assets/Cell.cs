2c6d9822448f448839492075757d60cfdf408d62
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 02:11:27 +0200

    Add Cell Script

---
+++ Assets/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+	public int cellX;
+	public int cellY;
+	public bool HasChiled()
+	{
+		return transform.childCount > 0;
+	}
+
+	public List<Transform> GetChild()
+	{
+		List<Transform> allChiled = new List<Transform>();
+		foreach (Transform chiled in transform)
+		{
+			allChiled.Add(chiled);
+		}
+		return allChiled;
+	}
+}
