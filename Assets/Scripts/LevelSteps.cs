039a44e9ce6890cb2416a32b8f62ae7e9521c7a3
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 02:13:20 +0200

    Create Level Script for Generate Levels

---
+++ Assets/Scripts/LevelSteps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewLevelSteps", menuName = "Level/Steps")]
+public class LevelSteps : ScriptableObject
+{
+    public List<LevelStep> Steps = new List<LevelStep>();
+}
+[Serializable]
+public class LevelStep
+{
+    public string cellTag;
+    public bool addToLastCellNeighbor;
+    public bool addToSpecificCell;
+    public IntVector2 cellCoordinated;
+    public bool addSetOfRandomCells;
+    public int amountOfRandomCells;
+}
