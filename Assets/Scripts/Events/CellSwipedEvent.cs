1e3b2db9c1258d14ab11d4ee86c804d316c6f8f3
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 04:55:39 +0200

    Add Event to fire when swipe finished

---
+++ Assets/Scripts/Events/CellSwipedEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "CellSwipedEvent", menuName = "Events/CellSwipedEvent")]
+public class CellSwipedEvent : Observable<CellSwipedEvent>
+{
+    public Cell targetCell;
+    public override void SetData(params object[] data)
+    {
+        targetCell = (Cell)data[0];
+    }
+}
