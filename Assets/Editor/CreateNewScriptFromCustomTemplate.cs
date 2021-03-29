c54c78b114e8840c1dba6a5be231b1fb12823d43
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 02:11:52 +0200

    Add Editor for create Event Scripts

---
+++ Assets/Editor/CreateNewScriptFromCustomTemplate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+public class CreateNewScriptFromCustomTemplate
+{
+    private const string eventTemplatePath = "Assets/Editor/GameEvent.cs.txt";
+
+    [MenuItem(itemName: "Assets/Create/Class/Create Event Class", isValidateFunction: false, priority: 51)]
+    public static void CreateEventTemplate()
+    {
+        ProjectWindowUtil.CreateScriptAssetFromTemplateFile(eventTemplatePath, "GameEvent.cs");
+    }
+}
