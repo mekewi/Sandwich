4b409bda515e594e44f964c5c11e68ea58186311
mohamed rashad <m.rashad.sm@gmail.com>
2021-03-29 04:57:44 +0200

    create main ui script to manage switching between  game scenes

---
+++ Assets/Scripts/MainSceneUI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneUI : MonoBehaviour
+{
+    public void Play2024Game()
+    {
+        SceneManager.LoadScene("2014Game");
+    }
+    public void PlayIngredientsGame()
+    {
+        SceneManager.LoadScene("IngredientsGame");
+    }
+}
